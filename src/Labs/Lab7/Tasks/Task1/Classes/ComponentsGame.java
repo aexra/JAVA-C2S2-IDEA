47041a9a623c7b307b8741e3f5d0aab8204ef4c7
Никита Фомин <defdXs@yandex.ru>
2024-05-09 18:46:15 +0300

    Refactoring

--- src/Labs/Lab7/Tasks/Task1/Classes/ComponentsGame.java
+++ src/Labs/Lab7/Tasks/Task1/Classes/ComponentsGame.java
@@ -7,7 +7,30 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ComponentsGame extends Game {
-    public ComponentsGame(GamePanel panel) {
-        super(panel, 1000, 600);
+    private JComboBox<Integer> comboBox;
+    private JLabel label;
+    private JButton button;
+    private JCheckBox checkbox;
+    private JRadioButton radio;
+    private JTable table;
+
+    public ComponentsGame() {
+        super();
+
+        comboBox = new JComboBox<>();
+        label = new JLabel();
+        button = new JButton("Hello, world!");
+        checkbox = new JCheckBox();
+        radio = new JRadioButton();
+        table = new JTable();
+
+        panel.add(comboBox);
+        panel.add(label);
+        panel.add(button);
+        panel.add(checkbox);
+        panel.add(radio);
+        panel.add(table);
+
+        panel.revalidate();
     }
 }
--- src/Labs/Lab7/Tasks/Task1/T1.java
+++ src/Labs/Lab7/Tasks/Task1/T1.java
@@ -1,10 +1,9 @@
 package Labs.Lab7.Tasks.Task1;
 
 import Labs.Lab7.Tasks.Task1.Classes.ComponentsGame;
-import Labs.Lab7.Tasks.Task1.Classes.ComponentsGamePanel;
 
 public class T1 {
     public static void main(String[] args) {
-        new ComponentsGame(new ComponentsGamePanel()).run();
+        new ComponentsGame().run();
     }
 }
