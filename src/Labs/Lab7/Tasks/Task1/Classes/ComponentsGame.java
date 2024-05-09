1a992c30f6619d01041aa4ef1d96d5a63d67ac27
Никита Фомин <defdXs@yandex.ru>
2024-05-09 19:04:37 +0300

    L7T1 Done

--- src/Labs/Lab7/Tasks/Task1/Classes/ComponentsGame.java
+++ src/Labs/Lab7/Tasks/Task1/Classes/ComponentsGame.java
@@ -4,10 +4,15 @@ import Labs.GameEngine.Core.Base.Game;
 import Labs.GameEngine.Core.Base.GamePanel;
 
 import javax.swing.*;
+import javax.swing.table.JTableHeader;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class ComponentsGame extends Game {
-    private JComboBox<Integer> comboBox;
+    private JComboBox<UIManager.LookAndFeelInfo> comboBox;
     private JLabel label;
     private JButton button;
     private JCheckBox checkbox;
@@ -18,11 +23,39 @@ public class ComponentsGame extends Game {
         super();
 
         comboBox = new JComboBox<>();
-        label = new JLabel();
+        label = new JLabel("This is label");
         button = new JButton("Hello, world!");
         checkbox = new JCheckBox();
         radio = new JRadioButton();
-        table = new JTable();
+        table = new JTable(
+                new String[][] {
+                        { "Си", "Деннис Ритчи", "1972" },
+                        { "C++", "Бьерн Страуструп", "1983" },
+                        { "Python", "Гвидо ван Россум", "1991" },
+                        { "Java", "Джеймс Гослинг", "1995" },
+                        { "JavaScript", "Брендон Айк", "1995" },
+                        { "C#", "Андерс Хейлсберг", "2001" },
+                        { "Scala", "Мартин Одерски", "2003" }
+                },
+                new String[] { "Язык", "Автор", "Год" }
+        );
+
+        var lafs = UIManager.getInstalledLookAndFeels();
+        for (var laf : lafs) {
+            comboBox.addItem(laf);
+        }
+        comboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    try {
+                        UIManager.setLookAndFeel(((UIManager.LookAndFeelInfo)e.getItem()).getClassName());
+                    } catch (Exception _) {
+
+                    }
+                }
+            }
+        });
 
         panel.add(comboBox);
         panel.add(label);
